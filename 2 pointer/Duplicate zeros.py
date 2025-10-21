3be0b037a1665f52aa71d02092c20440a9a36850
Rahul Kumar <rahulkumar.datascientist@gmail.com>
2025-10-21 14:23:37 +0100

    2 pointer/ duplicate zeros n times and shift left or right

--- 2 pointer/Duplicate zeros.py
+++ 2 pointer/Duplicate zeros.py
@@ -21,10 +21,52 @@ class Solution:
                 if right_index <= len(arr) - 1:
                     arr[right_index] = arr[left_index]
                 right_index-=1
-            left_index-=1   
-    
-    def duplicateZerosNspacesShiftRight(self, arr: list[int]) -> None:
+            left_index-=1
+   
+    # duplicate n times and shift right
+    def duplicateZerosNspacesShiftRight(self, arr: list[int], n: int) -> None:
+        zero_count = 0
+        for element in arr:
+            if element == 0:
+                zero_count+=n
         
+        left_index = len(arr)-1
+        right_index = left_index + zero_count
+
+        while left_index!=right_index:
+            if right_index<=len(arr)-1:
+                arr[right_index] = arr[left_index]
+            right_index-=1
+
+            if arr[left_index] == 0:
+                for i in range(n):
+                    if right_index<=len(arr)-1:
+                        arr[right_index] = arr[left_index]
+                    right_index-=1     
+            left_index-=1
+    
+    # duplicate n times and shift left
+    def duplicateZerosNspacesShiftLeft(self, arr: list[int], n: int) -> None:
+        zero_count = 0
+        for value in arr:
+            if value == 0:
+                zero_count+=n
+        
+        right_index = 0
+        left_index = 0 - zero_count
+
+        length = len(arr)
+        while left_index < length:
+            if left_index >= 0:
+                arr[left_index] = arr[right_index]
+            left_index+=1
+
+            if arr[right_index] == 0:
+                for i in range(n):
+                    if left_index >= 0:
+                        arr[left_index] = arr[right_index]
+                    left_index+=1
+            right_index+=1
 
 
 
@@ -32,12 +74,42 @@ class Solution:
 solution = Solution()
 
 arr = [1,0,2,3,0,4,5,0]
-print(solution.duplicateZerosOnceShiftRight(arr))
+print(arr, "\n-----------------------------------")
+
+# solution.duplicateZerosOnceShiftRight(arr)
+# print(arr)
 
 arr = [1,0,2,3,0,4,5,0]
-print(solution.duplicateZerosOnceShiftRight(arr))
+n = 1
+solution.duplicateZerosNspacesShiftRight(arr, n)
+print(arr)
 
+arr = [1,0,2,3,0,4,5,0]
+n = 2
+solution.duplicateZerosNspacesShiftRight(arr, n)
+print(arr)
 
+arr = [1,0,2,3,0,4,5,0]
+n = 3
+solution.duplicateZerosNspacesShiftRight(arr, n)
+print(arr)
+
+print("\n-----------------------------------")
+
+arr = [1,0,2,3,0,4,5,0]
+n = 1
+solution.duplicateZerosNspacesShiftLeft(arr, n)
+print(arr)
+
+arr = [1,0,2,3,0,4,5,0]
+n = 2
+solution.duplicateZerosNspacesShiftLeft(arr, n)
+print(arr)  
+
+arr = [1,0,2,3,0,4,5,0]
+n = 3
+solution.duplicateZerosNspacesShiftLeft(arr, n)
+print(arr)
 
             
 
