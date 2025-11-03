f3e6028e0f823e8d8da9b977aac9ecd1c8e09b2f
Rahul Kumar <rahulkumar.datascientist@gmail.com>
2025-11-03 13:10:02 +0000

    easy

---
+++ 2 pointer/Valid Palindrome II.py
@@ -0,0 +1,60 @@
+'''
+680. Valid Palindrome II
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+Example 1:
+Input: s = "aba"
+Output: true
+
+Example 2:
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+
+Example 3:
+Input: s = "abc"
+Output: false
+ 
+Constraints:
+
+1 <= s.length <= 10^5
+s consists of lowercase English letters.
+'''
+
+
+class Solution:
+    def isPalindrome(self, s, left, right):
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left+=1
+                right-=1
+            return True
+    
+    def validPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s)-1
+
+        while left < right:
+            if s[left] != s[right]:
+                return self.isPalindrome(s, left+1, right) or self.isPalindrome(s, left, right-1)
+            left+=1
+            right-=1
+        return True
+
+
+
+solution = Solution()
+
+s = "aba"
+print(solution.validPalindrome(s), "\t", s)
+
+s = "abca"
+print(solution.validPalindrome(s), "\t", s)
+
+s = "abc"
+print(solution.validPalindrome(s), "\t", s)
+
+s = "deeee"
+print(solution.validPalindrome(s), "\t", s)
+
